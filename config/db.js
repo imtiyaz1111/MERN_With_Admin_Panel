5e88c977f421660d204320d3a6172a5cda45e128
Md Imtiyaz Alam <118051060+imtiyaz1111@users.noreply.github.com>
2024-04-16 08:03:22 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-const DB=process.env.DATABASE
+const DB="mongodb://imtiyazalam:imtiyazalam@ac-osmbmim-shard-00-00.oplf40u.mongodb.net:27017,ac-osmbmim-shard-00-01.oplf40u.mongodb.net:27017,ac-osmbmim-shard-00-02.oplf40u.mongodb.net:27017/MernWithAdmin?ssl=true&replicaSet=atlas-tfer3k-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 const connectToDb=async  ()=>{
     mongoose.connect(DB)
